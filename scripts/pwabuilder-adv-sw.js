ba599f4f373eecfca00e0787c4cfd1f904c866ca
anthrline <anthrline@gmail.com>
2024-03-05 14:02:52 +0100

    Adding serviceworker and manifest

---
+++ scripts/pwabuilder-adv-sw.js
@@ -0,0 +1,3 @@
+
+  import { precacheAndRoute } from 'workbox-precaching/precacheAndRoute';
+  precacheAndRoute([{"revision":"743d95f3488a5bd7b420d79aface9c7b","url":"index.html"},{"revision":"61b575a279c06b731c8423919b104eae","url":"loading.html"},{"revision":"498dae9cb18103867275f96f0c327968","url":"scripts/loader.js"},{"revision":"42e52b9a6457a86894696001d79e914e","url":"scripts/script.js"},{"revision":"e71bfecb58f82462fd7e104cfe98ce16","url":"styles/loading.css"},{"revision":"74c83d0d31c947e56eb7d56a0d661106","url":"styles/style.css"}]);
